5bd8132231290dc605cfe36d50dc54ee929de5f5
tusharmishra57 <tusharmishra.developer@gmail.com>
2026-07-16 19:52:19 +0530

    routing skeleton

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+const app = express();
+
+app.post("/api/v1/signup", (res, req) => {
+     
+})
+
+app.post("/api/v1/signin", (res, req) => {
+    
+})
+
+app.post("/api/v1/content", (res, req) => {
+    
+})
+
+app.get("/api/v1/content", (res, req) => {
+    
+})
+
+app.delete("/api/v1/delete", (res, req) => {
+    
+})
+
+app.post("/api/v1/brain/share", (res, req) => {
+
+})
+
+app.get("/api/v1/brain/shareLink", (res, req) =>{
+
+})
+
