32ece0871b6bc55718b1463e35b193799b0bde36
tusharmishra57 <tusharmishra.developer@gmail.com>
2026-07-19 21:22:46 +0530

    Generic Card Component

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -5,13 +5,21 @@ import { ShareIcon } from "./icons/SharIcon"
 
 function App() {
   return <div>
-    <Button variant="primary" text="Add content" startIcon={<PlusIcon/>}></Button>
-    <Button variant="secondary" text="Share Brain" startIcon={<ShareIcon/>}></Button>
+    <div className="flex justify-end">
+      <div className= "p-3">
+        <Button variant="primary" text="Add content" startIcon={<PlusIcon/>}></Button>
+      </div>
+      <div className= "p-3">
+        <Button variant="secondary" text="Share Brain" startIcon={<ShareIcon/>}></Button>
+      </div>
+    </div>
+    
 
-    <Card>
-
+    <Card title = "youtube video" link="https://www.youtube.com/watch?v=ZqGSg4b_cZA&t=1s" type="youtube">
     </Card>
 
+    <Card title = "twitter " link="https://x.com/AlphaLabx/status/2078813938503069799?s=20" type="twitter">
+    </Card>
   </div>
 }
 
--- Frontend/src/components/Card.tsx
+++ Frontend/src/components/Card.tsx
@@ -1,14 +1,21 @@
 import { ShareIcon } from "../icons/SharIcon"
 
-export const Card = () =>{
-    return <div className="rounded-md shadow-lg outline-slate-50 outline-solid m-3 p-4 w-84 h-64 bg-blue-300">
+interface CardProps {
+    title: string;
+    link: string;
+    type: "twitter" | "youtube"
+}
+
+
+export const Card = (props: CardProps) =>{
+    return <div className="rounded-md shadow-lg outline-slate-50 outline-solid m-3 p-4 w-84 bg-blue-300">
         <div className=" flex justify-between">
         <div className="pl-1 flex items-center h-5">
             <div className="mr-2 ml-2 text-gray-400">
                 <ShareIcon/>
             </div>
             <div className="mr-2 ml-2"> 
-                Project Ideas
+                {props.title}
             </div>
             
         </div>
@@ -17,16 +24,24 @@ export const Card = () =>{
                 <ShareIcon/>
             </div>
             <div className="mr-2 ml-2 text-gray-400">
-                <ShareIcon/>
+                <a href={props.link} target="_blank">
+                    <ShareIcon/>
+                </a>
             </div>
         </div>
 
     </div>
-    <div className="w-full pt-4 ">
-            {/* <iframe src="https://www.youtube.com/embed/ZqGSg4b_cZA?si=QpZkQAX6Dr_LDvaq" 
-            title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
-            gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe> */}
-                
+    <div className="pt-2 ">
+            {props.type === "youtube" && <iframe src={props.link.replace("watch", "embed")}
+            title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; 
+            clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+            referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>}
+
+
+            {props.type === "twitter" && <blockquote className="twitter-tweet">
+            <a href={props.link} target="_blank"></a>
+            </blockquote>
+                }
         </div>
     </div>
     
