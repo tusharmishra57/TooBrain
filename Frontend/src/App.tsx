1539acf2baa7ee2ba81676ccff60cea39ba07e53
tusharmishra57 <tusharmishra.developer@gmail.com>
2026-07-20 19:15:08 +0530

    React Routing

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -6,6 +6,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
 return <div>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/signup" element={<Signup/>}></Route>
+      <Route path="/signin" element={<Signin/>}></Route>
+      <Route path="/dashboard" element={<Dashboard/>}></Route>
+    </Routes>
+  </BrowserRouter>
+</div>
     
 }
 
